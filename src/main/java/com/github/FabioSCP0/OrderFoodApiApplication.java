9aa42f454078115a6aeec3e752c4a58514d75e1e
Fabio Silva <fabio.s0@hotmail.com>
2022-07-26 14:39:20 -0300

    Iniciando o projeto Spring Boot

---
+++ src/main/java/com/github/FabioSCP0/OrderFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.FabioSCP0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFoodApiApplication.class, args);
+	}
+
+}
